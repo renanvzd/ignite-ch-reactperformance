efbb3981cf23efdd39fedea6b51f9af67c3af011
Renan Veronez Drechsler <renanvz@outlook.com>
2021-12-19 17:05:14 -0300

    useMemo

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { ProductItem } from "./ProductItem"
 
 interface SearchResultsProps {
@@ -9,8 +10,13 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = results.reduce((total, product) => {
+    return total + product.price;
+  }, 0)
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
       {results.map(product => {
         return (
           <div key={product.id}>
