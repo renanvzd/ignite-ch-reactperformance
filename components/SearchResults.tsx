21224d019a517564d95de8a3fcf726d78afdedc3
Renan Veronez Drechsler <renanvz@outlook.com>
2021-12-20 14:40:14 -0300

    Virtualizacao

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { List, ListRowRenderer } from 'react-virtualized';
 import { ProductItem } from "./ProductItem"
 
 interface SearchResultsProps {
@@ -13,20 +13,29 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ totalPrice, results, onAddToWishList }: SearchResultsProps) {
-
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    );
+  }
 
   return (
     <div>
       <h2>{totalPrice}</h2>
-      {results.map(product => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            onAddToWishList={onAddToWishList}
-          />
-        )
-      })}
+
+      <List
+        height={300}
+        rowHeight={30}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   )
 }
